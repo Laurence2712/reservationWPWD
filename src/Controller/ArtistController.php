4cb1e782c4c215f6adc66604f35f405ac996b25d
Laurence <laurencepirard27@gmail.com>
2020-05-07 10:50:50 +0200

    Modèle Location + Relation ManyToOne with Locality

--- src/Controller/ArtistController.php
+++ src/Controller/ArtistController.php
@@ -13,26 +13,28 @@ class ArtistController extends AbstractController
      */
     public function index()
     {
+        $titre = 'Liste des artistes';
+        
         $repository = $this->getDoctrine()->getRepository(Artist::class);
         $artists = $repository->findAll();
-
+        
         return $this->render('artist/index.html.twig', [
+            'titre' => $titre,
             'artists' => $artists,
-            'resource' =>'artistes',
         ]);
     }
-
+    
     /**
+     * 
      * @Route("/artist/{id}", name="artist_show")
+     * @param type $id
      */
-    public function show($id)
-    {
-        $repository = $this->getDoctrine()->getRepository(Artist::class);
-        $artist = $repository->find($id);
-
-        return $this->render('artist/show.html.twig', [
+    public function show(Artist $artist) {
+        $titre = 'Fiche artiste';
+        
+        return $this->render('artist/show.html.twig',[
+            'titre' => $titre,
             'artist' => $artist,
-            
         ]);
     }
 }
